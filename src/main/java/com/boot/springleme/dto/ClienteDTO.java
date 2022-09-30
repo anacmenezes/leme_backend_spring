b8369275c5eded7f77d5624391be17a3f89e34d3
anacmenezes <anamenezes.dev@gmail.com>
2022-09-30 11:38:41 -0300

    CRUD finalizado

--- src/main/java/com/boot/springleme/dto/ClienteDTO.java
+++ src/main/java/com/boot/springleme/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.boot.springleme.domain.Cliente;
+import com.boot.springleme.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
