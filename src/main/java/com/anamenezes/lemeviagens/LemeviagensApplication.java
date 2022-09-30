a27348a697a63f44de2babf7ba4a6a107a5eb853
anacmenezes <anamenezes.dev@gmail.com>
2022-09-30 10:07:57 -0300

    Projeto criado

---
+++ src/main/java/com/anamenezes/lemeviagens/LemeviagensApplication.java
@@ -0,0 +1,13 @@
+package com.anamenezes.lemeviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemeviagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemeviagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anamenezes/lemeviagens/LemeviagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anamenezes.lemeviagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemeviagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
